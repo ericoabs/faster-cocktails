cc5626b3c3fae1d1a7efb764afd6d0ecbca6ddbe
Erico Alexandre <erico.alexandre@jointecnologia.com.br>
2023-07-29 15:54:38 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="container mx-auto px-4 py-8">
+    <header class="mb-8 text-center">
+      <h1 class="text-3xl font-semibold text-gray-800">
+        Bem-vindo à Faster Cocktails
+      </h1>
+    </header>
+
+    <section class="flex flex-wrap -mx-4">
+    </section>
+
+    <footer class="mt-8 text-center">
+      <p class="text-gray-500">
+        © 2023 Todos os direitos reservados.
+      </p>
+    </footer>
+  </div>
+</template>
