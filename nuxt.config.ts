2a046807bdb2f48de02e958b07f5ac9dc9bcf88b
Erico Alexandre <erico.alexandre@jointecnologia.com.br>
2023-07-30 13:44:49 -0300

    feature: adicionar pacote para exibição de notificações toast

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  css: ['vue-toast-notification/dist/theme-default.css'],
   modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module']
 })
